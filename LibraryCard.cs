7c896927afd854b1bbdf24025408609f23c26458
Yousef <youssefahmedelesawy@gmail.com>
2025-08-22 02:31:21 +0300

    initial commit

---
+++ LibraryCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frist_Project
+{
+    internal class LibraryCard
+    {
+
+        public int Number {  get; set; }
+    }
+}
