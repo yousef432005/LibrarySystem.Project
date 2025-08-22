7725f5252475b9db36313b30b283b5b0701eabe5
Yousef <youssefahmedelesawy@gmail.com>
2025-08-22 05:10:48 +0300

    Save local changes before pull

--- obj/Debug/net8.0/LibrarySystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibrarySystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92514856444135420e546c529029aa78d04eecf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
